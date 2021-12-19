57f6e292d0b3627da24b6630d4416077e2216d43
Nikolay Ulyanov <ulyanick@gmail.com>
2021-12-19 18:11:52 +0100

    Update Embedder.py
    Make it more like the code i use everyday

--- Perceptor/Embedder.py
+++ Perceptor/Embedder.py
@@ -9,6 +9,44 @@ import kornia.augmentation as K
 
 PADDING_MODES = {'mirror':'reflect','smear':'replicate','wrap':'circular','black':'constant'}
 
+def lanczos(x, a):
+  cond = torch.logical_and(-a < x, x < a)
+  out = torch.where(cond, sinc(x) * sinc(x/a), x.new_zeros([]))
+  return out / out.sum()
+  
+
+def ramp(ratio, width):
+  n = math.ceil(width / ratio + 1)
+  out = torch.empty([n])
+  cur = 0
+  for i in range(out.shape[0]):
+    out[i] = cur
+    cur += ratio
+  return torch.cat([-out[1:].flip([0]), out])[1:-1]
+
+
+def resample(input, size, align_corners=True):
+  n, c, h, w = input.shape
+  dh, dw = size
+
+  input = input.view([n * c, 1, h, w])
+
+  if dh < h:
+    kernel_h = lanczos(ramp(dh / h, 2), 2).to(input.device, input.dtype)
+    pad_h = (kernel_h.shape[0] - 1) // 2
+    input = F.pad(input, (0, 0, pad_h, pad_h), 'reflect')
+    input = F.conv2d(input, kernel_h[None, None, :, None])
+
+  if dw < w:
+    kernel_w = lanczos(ramp(dw / w, 2), 2).to(input.device, input.dtype)
+    pad_w = (kernel_w.shape[0] - 1) // 2
+    input = F.pad(input, (pad_w, pad_w, 0, 0), 'reflect')
+    input = F.conv2d(input, kernel_w[None, None, None, :])
+
+  input = input.view([n, c, h, w])
+  return F.interpolate(input, size, mode='bicubic', align_corners=align_corners)
+
+
 class HDMultiClipEmbedder(nn.Module):
   """
   Multi-CLIP embedder that uses cutouts to view images larger than 224x224.
@@ -16,18 +54,18 @@ class HDMultiClipEmbedder(nn.Module):
   and jbusted (https://twitter.com/jbusted1)
   and dribnet (https://github.com/dribnet)
   """
-  def __init__(self, perceptors=None, cutn = 40, cut_pow = 1.5, padding = 0.25, border_mode = 'clamp', noise_fac = 0.1):
+  def __init__(self, perceptors=None, cutn = 64, cut_pow = 1.0, padding = 0.25, border_mode = 'clamp', noise_fac = 0.1):
     super().__init__()
     if perceptors is None:
       perceptors = pytti.Perceptor.CLIP_PERCEPTORS
     self.cut_sizes = [p.visual.input_resolution for p in perceptors]
     self.cutn = cutn
     self.noise_fac = noise_fac
-    self.augs = nn.Sequential(K.RandomHorizontalFlip(p=0.3),
+    self.augs = nn.Sequential(K.RandomHorizontalFlip(p=0.5),
+                              K.RandomSharpness(0.3, p=0.4),
                               K.RandomAffine(degrees=30, translate=0.1, p=0.8, padding_mode='border'),
                               K.RandomPerspective(0.2, p=0.4,),
-                              K.ColorJitter(hue=0.01, saturation=0.01,  p=0.7),
-                              K.RandomErasing(scale=(.1, .4), ratio=(.3, 1/.3), same_on_batch=False, p=0.7),
+                              K.ColorJitter(hue=0.01, saturation=0.01, p=0.7),
                               nn.Identity(),)
     self.input_axes  = ('n', 's', 'y', 'x')
     self.output_axes = ('c', 'n', 'i')
@@ -35,45 +73,9 @@ class HDMultiClipEmbedder(nn.Module):
     self.padding = padding
     self.cut_pow = cut_pow
     self.border_mode = border_mode
-
-  def make_cutouts(self, input, side_x, side_y, cut_size, device = DEVICE):
-    min_size = min(side_x, side_y, cut_size)
-    max_size = min(side_x, side_y)
-    paddingx = min(round(side_x * self.padding), side_x)
-    paddingy = min(round(side_y * self.padding), side_y)
-    cutouts = []
-    offsets = []
-    sizes   = []
-    for _ in range(self.cutn):
-      #mean is 0.8
-      #varience is 0.3
-      size = int(max_size *
-            (torch.zeros(1,).normal_(mean=.8, std=.3)
-            .clip(cut_size/max_size, 1.) ** self.cut_pow))
-      offsetx_max = side_x - size + 1
-      offsety_max = side_y - size + 1
-      if self.border_mode == 'clamp':
-        offsetx = torch.clamp((torch.rand([])*(offsetx_max+2*paddingx) - paddingx).floor().int(), 0, offsetx_max)
-        offsety = torch.clamp((torch.rand([])*(offsety_max+2*paddingy) - paddingy).floor().int(), 0, offsety_max)
-        cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
-      else:
-        px = min(size, paddingx)
-        py = min(size, paddingy)
-        offsetx = (torch.rand([])*(offsetx_max+2*px) - px).floor().int()
-        offsety = (torch.rand([])*(offsety_max+2*py) - py).floor().int()
-        cutout = input[:, :, paddingy + offsety:paddingy + offsety + size, paddingx + offsetx:paddingx + offsetx + size]
-      cutouts.append(F.adaptive_avg_pool2d(cutout, cut_size))
-      offsets.append(torch.as_tensor([[offsetx/side_x, offsety/side_y]]).to(device))
-      sizes.append(torch.as_tensor([[size/side_x, size/side_y]]).to(device))
-    cutouts = self.augs(torch.cat(cutouts))
-    offsets = torch.cat(offsets)
-    sizes   = torch.cat(sizes)
-    if self.noise_fac:
-      facs    = cutouts.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
-      cutouts.add_(facs * torch.randn_like(cutouts))
-    return cutouts, offsets, sizes
-
-  def forward(self, diff_image, input = None, device = DEVICE):
+    
+    
+    def forward(self, diff_image, input = None, device = DEVICE):
     """
     diff_image: (DifferentiableImage) input image
     returns images embeds
@@ -85,18 +87,29 @@ class HDMultiClipEmbedder(nn.Module):
     else:
       input = format_input(input, diff_image, self).to(device = device, memory_format = torch.channels_last)
     max_size = min(side_x, side_y)
+    min_size = min(side_x, side_y, self.cut_sizes[0])
+    cutouts = []
+    offsets = []
+    sizes   = []
+    for _ in range(self.cutn):
+        size = int(torch.rand([])**self.cut_pow * (max_size - min_size) + min_size)
+        offsetx = torch.randint(0, side_x - size + 1, ())
+        offsety = torch.randint(0, side_y - size + 1, ())
+        cutout = input[:, :, offsety:offsety + size, offsetx:offsetx + size]
+        cutouts.append(resample(cutout, (self.cut_size, self.cut_size)))
+        offsets.append(torch.as_tensor([[offsetx / side_x, offsety / side_y]]).to(device))
+        sizes.append(torch.as_tensor([[size / side_x, size / side_y]]).to(device))
+    cutouts = self.augs(torch.cat(cutouts, dim=0))
+    offsets = torch.cat(offsets)
+    sizes   = torch.cat(sizes)
+    if self.noise_fac:
+      facs = cutouts.new_empty([self.cutn, 1, 1, 1]).uniform_(0, self.noise_fac)
+      cutouts.add_(facs * torch.randn_like(cutouts))
+    clip_in = normalize(cutouts)
     image_embeds = []
     all_offsets = []
     all_sizes = []
-
-    paddingx = min(round(side_x * self.padding), side_x)
-    paddingy = min(round(side_y * self.padding), side_y)
-    if self.border_mode != 'clamp':
-      input = F.pad(input,(paddingx, paddingx, paddingy, paddingy), mode = PADDING_MODES[self.border_mode])
-    for cut_size, perceptor in zip(self.cut_sizes, perceptors):
-      cutouts, offsets, sizes = self.make_cutouts(input, side_x, side_y, cut_size)
-      clip_in = normalize(cutouts)
-      image_embeds.append(perceptor.encode_image(clip_in).float().unsqueeze(0))
-      all_offsets.append(offsets)
-      all_sizes.append(sizes)
+    image_embeds.append(perceptors[0].encode_image(clip_in).float().unsqueeze(0))
+    all_offsets.append(offsets)
+    all_sizes.append(sizes)
     return cat_with_pad(image_embeds), torch.stack(all_offsets), torch.stack(all_sizes)
