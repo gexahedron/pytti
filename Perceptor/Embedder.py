9b8508537f0e8f10780c0027b2c9b308917e7889
Nikolay Ulyanov <ulyanick@gmail.com>
2021-12-19 18:33:45 +0100

    Update Embedder.py

--- Perceptor/Embedder.py
+++ Perceptor/Embedder.py
@@ -10,6 +10,10 @@ import kornia.augmentation as K
 
 PADDING_MODES = {'mirror':'reflect','smear':'replicate','wrap':'circular','black':'constant'}
 
+def sinc(x):
+  return torch.where(x != 0, torch.sin(math.pi * x) / (math.pi * x), x.new_ones([]))
+
+
 def lanczos(x, a):
   cond = torch.logical_and(-a < x, x < a)
   out = torch.where(cond, sinc(x) * sinc(x/a), x.new_zeros([]))
